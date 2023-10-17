092b373009f60e716e92c62a8185b1d73ad85856
Alex Chaus <chausme@gmail.com>
2023-10-17 18:39:50 +0400

    Place JS into HTML and move <style> inside <head>

--- js/main.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('convert').addEventListener('click', function () {
-    const textarea = document.getElementById('content');
-    textarea.value = textarea.value.toLowerCase();
-});
