6090de5d0e17dc8b0f47b4d15c14b11e0a61aefc
Alex Chaus <chausme@gmail.com>
2023-10-17 16:45:25 +0400

    Add Bootstrap based version

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.getElementById('convert').addEventListener('click', function () {
+    const textarea = document.getElementById('content');
+    textarea.value = textarea.value.toLowerCase();
+});
